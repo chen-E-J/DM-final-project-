23f653d7e4821aafffba39e9cb7e566f81983332
chen-E-J <71671473+chen-E-J@users.noreply.github.com>
2021-02-28 12:31:20 -0500

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,9 @@
+CREATE TEAM07_final_project;
+
+USE TEAM07_final_project;
+
+CREATE TABLE zone
+    (Zone_id INT(8) NOT NULL AUTO_INCREMENT,
+    Descrip TEXT NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    PRIMARY KEY(Zone_id));
